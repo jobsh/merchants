b670e5a4cbff4fd55f46fcb20a9e977dba57728c
hanke <14703110113@163.com>
2020-10-28 09:54:11 +0800

    修改user_id 为id，修改一些配置

--- merchant-admin/src/main/java/com/merchant/RuoYiApplication.java
+++ merchant-admin/src/main/java/com/merchant/RuoYiApplication.java
@@ -1,8 +1,10 @@
 package com.merchant;
 
+import com.merchant.common.config.RuoYiConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * 启动程序
