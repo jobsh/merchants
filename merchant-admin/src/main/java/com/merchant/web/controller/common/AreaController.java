9dfd06b9ed2637040efe02a25dd12002cf264e3a
hanke <14703110113@163.com>
2020-11-20 18:26:50 +0800

    修改SysUserMapper.xml字段

--- merchant-admin/src/main/java/com/merchant/web/controller/common/AreaController.java
+++ merchant-admin/src/main/java/com/merchant/web/controller/common/AreaController.java
@@ -11,7 +11,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
